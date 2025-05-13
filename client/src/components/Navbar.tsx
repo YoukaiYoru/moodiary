40eb3b9ff91577bb8ad76764fd3765a1e354a8e8
Daniel <daniel.preciado.j@uni.pe>
2025-05-12 22:22:08 -0500

    first commit frontend

---
+++ client/src/components/Navbar.tsx
@@ -0,0 +1,17 @@
+
+function Navbar() {
+    return (
+        <>
+            
+            <nav className='navbar'>
+                <div className="flex justify-between items-center px-6 py-4 bg-white shadow-md"></div>
+                <button className="bg-indigo-600 text-white px-4 py-2 rounded hover:bg-indigo-500 transition">
+                    Iniciar Sesión
+                </button>
+            </nav>
+        </>
+    );
+    
+}
+
+export default Navbar
