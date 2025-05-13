abc80b2d046bc0c1318ff2a67111f2541c549af3
Daniel <daniel.preciado.j@uni.pe>
2025-05-13 18:47:08 -0500

    HomeLogin view

--- client/src/pages/Home.tsx
+++ client/src/pages/Home.tsx
@@ -3,44 +3,44 @@ import { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import Loader from "@/components/Loader";
 function Home() {
-  const { isSignedIn } = useUser();
-  const navigate = useNavigate();
-  const [loading, setLoading] = useState(true);
+    const { isSignedIn } = useUser();
+    const navigate = useNavigate();
+    const [loading, setLoading] = useState(true);
 
-  useEffect(() => {
-    if (isSignedIn) {
-      setLoading(true); // Inicia el loading
-      setTimeout(() => {
-        navigate("/dashboard", { replace: true });
-      }, 500); // Puedes ajustar el tiempo de transición
-    } else {
-      setLoading(false); // No es necesario el loading si no está logueado
+    useEffect(() => {
+        if (isSignedIn) {
+            setLoading(true); // Inicia el loading
+            setTimeout(() => {
+                navigate("/dashboard", { replace: true });
+            }, 500); // Puedes ajustar el tiempo de transición
+        } else {
+            setLoading(false); // No es necesario el loading si no está logueado
+        }
+    }, [isSignedIn, navigate]);
+
+    if (loading) {
+        return (
+            <div className="flex justify-center items-center h-screen">
+                <Loader isLoaded={loading} />
+            </div>
+        );
     }
-  }, [isSignedIn, navigate]);
 
-  if (loading) {
+    if (isSignedIn) return null;
+
     return (
-      <div className="flex justify-center items-center h-screen">
-        <Loader isLoaded={loading} />
-      </div>
+        <div className="h-screen flex justify-center items-center">
+            <div className="w-[40%] flex flex-col items-center rounded-4xl shadow-2xl p-6">
+                <h1 className="font-playwrite text-6xl pb-10">Moodiary</h1>
+                <p>
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Illo
+                    distinctio vel est culpa, iste reiciendis laudantium rerum. Ea,
+                    laboriosam recusandae!
+                </p>
+                <SignInButton forceRedirectUrl="/dashboard" />
+            </div>
+        </div>
     );
-  }
-
-  if (isSignedIn) return null;
-
-  return (
-    <div className="h-screen flex justify-center items-center">
-      <div className="w-[40%] flex flex-col items-center rounded-4xl shadow-2xl p-6">
-        <h1 className="font-playwrite text-6xl pb-10">Moodiary</h1>
-        <p>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Illo
-          distinctio vel est culpa, iste reiciendis laudantium rerum. Ea,
-          laboriosam recusandae!
-        </p>
-        <SignInButton forceRedirectUrl="/dashboard" />
-      </div>
-    </div>
-  );
 }
 
 export default Home;
--- client/src/pages/HomeLogin.tsx
+++ client/src/pages/HomeLogin.tsx
@@ -1,3 +1,58 @@
+import { Button } from '@/components/ui/button';
+import { Textarea } from '@/components/ui/textarea';
+import { TiLocationArrowOutline } from "react-icons/ti";
+
+
+
 export default function HomeLogin() {
-  return <h1 className="text-2xl font-bold">Bienvenido al Dashboard</h1>;
+  return (
+    <>
+      <h1 className="text-2xl font-light">Bienvenido al Dashboard</h1>
+
+      <div className='flex justify-center items-center min-h-[60vh]'>
+        <section className="container flex flex-col justify-center items-center">
+          <h1 className="font-dosis font-light text-5xl pt-4 pb-4" >¿Cómo te sientes hoy? 🫣</h1>
+          <div className='flex justify-center items-center'>
+            <p className='emotion-button'>😄</p>
+            <p className='emotion-button'>😍</p>
+            <p className='emotion-button'>😕</p>
+            <p className='emotion-button'>🫠</p>
+            <p className='emotion-button'>😤</p>
+            <p className='emotion-button'>😡</p>
+          </div>
+          <div className="relative w-[60vw] mt-6">
+            {/* <Input
+              className='h-[50px] w-full pr-16 rounded-2xl border-2 placeholder:text-lg hover:shadow-2xl'
+              placeholder='Escribe cómo te sientes hoy... por ejemplo: Estoy motivado y con energía'
+            /> */}
+
+            <Textarea
+              className="min-h-[50px] w-full pr-16 rounded-2xl border-2 placeholder:text-xl hover:shadow-2xl resize-none overflow-hidden"
+              placeholder="Escribe cómo te sientes hoy... Ej: Me siento agradecido y con energía"
+              onInput={(e) => {
+                const target = e.target as HTMLTextAreaElement;
+                target.style.height = 'auto';
+                target.style.height = `${target.scrollHeight}px`;
+              }}
+            />
+
+            <Button
+              className='absolute top-[50%] right-4 transform translate-y-[-50%] rounded-xl px-4 py-4 text-sm'
+            // onClick={addEmotion()}
+            >
+              <TiLocationArrowOutline />
+            </Button>
+          </div>
+          <Button
+            variant="outline"
+            className='mt-8 shadow-amber-100'
+          // onClick={addLuckyButton()}
+          >
+            Me siento con suerte
+          </Button>
+
+        </section>
+      </div>
+    </>
+  );
 }
