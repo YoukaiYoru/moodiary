c330bd0e578abfb28007fb9852038abb589a20a2
Daniel <daniel.preciado.j@uni.pe>
2025-05-14 20:22:19 -0500

    finish home view and improve dashboard (some functions pending)

--- client/src/pages/Home.tsx
+++ client/src/pages/Home.tsx
@@ -29,15 +29,18 @@ function Home() {
     if (isSignedIn) return null;
 
     return (
-        <div className="h-screen flex justify-center items-center">
-            <div className="w-[40%] flex flex-col items-center rounded-4xl shadow-2xl p-6">
-                <h1 className="font-playwrite text-6xl pb-10">Moodiary</h1>
-                <p>
-                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Illo
-                    distinctio vel est culpa, iste reiciendis laudantium rerum. Ea,
-                    laboriosam recusandae!
+        <div
+            className="h-screen flex justify-center items-center bg-[url(assets/images/bg_image.jpg)] bg-no-repeat bg-center bg-cover"
+        >
+            <div className="w-[40%] flex flex-col items-center rounded-4xl shadow-2xl p-6 bg-transparent backdrop-blur-xl">
+                <h1 className="font-playwrite text-6xl pt-8 pb-10">Moodiary</h1>
+                <p className="font-dosis leading-relaxed text-xl px-4">
+                    Registra cómo te sientes en cualquier momento del día y empieza a entender mejor tus emociones. Reflexiona, crece y cuida de ti mismo con solo unos minutos al día.
+                    Tu bienestar emocional importa, y aquí tienes un espacio seguro para expresarlo.
                 </p>
-                <SignInButton forceRedirectUrl="/dashboard" />
+                <SignInButton forceRedirectUrl="/dashboard" className="bg-amber-400 font-dosis text-center text-2xl px-4 py-3 mt-5 mb-1.5 rounded-2xl transform transition hover:scale-110 duration-200 cursor-pointer">
+                    Iniciar Sesión
+                </SignInButton>
             </div>
         </div>
     );
--- client/src/pages/HomeLogin.tsx
+++ client/src/pages/HomeLogin.tsx
@@ -1,39 +1,76 @@
 import { Button } from '@/components/ui/button';
 import { Textarea } from '@/components/ui/textarea';
+import type React from 'react';
+import { useEffect, useState } from 'react';
 import { TiLocationArrowOutline } from "react-icons/ti";
+import confetti from 'canvas-confetti';
 
 
 
 export default function HomeLogin() {
+  // Play emoji sound
+  const playSound = (soundName: string) => {
+    const audio = new Audio(`/sounds/${soundName}.mp3`);
+    audio.play();
+  }
+
+  // Contador de caracteres
+  const max_length = 250;
+  const [text, setText] = useState('');
+  const total_chars = text.length;
+  const words = text.trim() === '' ? 0 : text.split(/\s+/).length;
+  const remaining = max_length - total_chars;
+
+  const wordCounter = (e: React.ChangeEvent<HTMLTextAreaElement>) => {
+    const value = e.target.value;
+    setText(value);
+  }
+
+  // Feeling lucky button
+  const addLuckyButton = () => {
+    // Add some confetti
+    confetti({
+      particleCount: 200,
+      angle: 60,
+      spread: 180,
+      origin: { x: 0, y: 0.6 },
+    });
+    confetti({
+      particleCount: 200,
+      angle: 120,
+      spread: 180,
+      origin: { x: 1, y: 0.6 },
+    });
+
+  }
+
+
   return (
     <>
       <h1 className="text-2xl font-light">Bienvenido al Dashboard</h1>
 
-      <div className='flex justify-center items-center min-h-[60vh]'>
+      <div className='flex justify-center items-center min-h-[80vh]'>
         <section className="container flex flex-col justify-center items-center">
           <h1 className="font-dosis font-light text-5xl pt-4 pb-4" >¿Cómo te sientes hoy? 🫣</h1>
           <div className='flex justify-center items-center'>
-            <p className='emotion-button'>😄</p>
-            <p className='emotion-button'>😍</p>
-            <p className='emotion-button'>😕</p>
-            <p className='emotion-button'>🫠</p>
-            <p className='emotion-button'>😤</p>
-            <p className='emotion-button'>😡</p>
+            <p className='emotion-button' onClick={() => playSound('happy')}>😄</p>
+            <p className='emotion-button' onClick={() => playSound('happy')}>😍</p>
+            <p className='emotion-button' onClick={() => playSound('happy')}>😕</p>
+            <p className='emotion-button' onClick={() => playSound('happy')}>🫠</p>
+            <p className='emotion-button' onClick={() => playSound('happy')}>😤</p>
+            <p className='emotion-button' onClick={() => playSound('happy')}>😡</p>
           </div>
           <div className="relative w-[60vw] mt-6">
-            {/* <Input
-              className='h-[50px] w-full pr-16 rounded-2xl border-2 placeholder:text-lg hover:shadow-2xl'
-              placeholder='Escribe cómo te sientes hoy... por ejemplo: Estoy motivado y con energía'
-            /> */}
-
             <Textarea
-              className="min-h-[50px] w-full pr-16 rounded-2xl border-2 placeholder:text-xl hover:shadow-2xl resize-none overflow-hidden"
+              className="min-h-[50px] w-full pr-16 rounded-2xl border-2 text-lg sm:text-lg md:text-xl  placeholder:text-xl hover:shadow-2xl resize-none overflow-hidden"
               placeholder="Escribe cómo te sientes hoy... Ej: Me siento agradecido y con energía"
               onInput={(e) => {
                 const target = e.target as HTMLTextAreaElement;
                 target.style.height = 'auto';
                 target.style.height = `${target.scrollHeight}px`;
               }}
+              onChange={wordCounter}
+              maxLength={max_length}
             />
 
             <Button
@@ -43,10 +80,17 @@ export default function HomeLogin() {
               <TiLocationArrowOutline />
             </Button>
           </div>
+
+          {/* Contadores */}
+          <div className='flex justify-end items-center w-[60vw] mt-4'>
+            <p className='text-sm font-dosis px-2'>Palabras: {words}</p>
+            <p className='text-sm font-dosis px-2'>Caracteres: {total_chars}/{remaining}</p>
+          </div>
+
           <Button
             variant="outline"
-            className='mt-8 shadow-amber-100'
-          // onClick={addLuckyButton()}
+            className='mt-2 shadow-amber-100 cursor-pointer hover:shadow hover:scale-110'
+            onClick={addLuckyButton}
           >
             Me siento con suerte
           </Button>
